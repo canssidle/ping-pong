38afeb3556fbc57b7f75a659ce5b64fc94301d89
canssidle wairimu <canssidlewairimu@gmail.com>
2018-03-30 21:32:22 +0300

    added README.md

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,36 @@
-// user interface logic
-document.ready(function){
-$("form#pingpong").submit (function(event)){  
- event.preventDefault;
-  var inputNum = parseInt($("input#num").val());
-   
- });
-});
-// business logic
+//backend
 
 function getPingPongArray(inputNum){
-  var PingPongArray=[];
-  for (var i=0; i<inputNum; i +1){
-    if(i%15===0){
-      PingPongArray.push('pingpong');
-    {
-    else if(i%3===0){
-      PingPongArray.push('ping');
-    }
-    else if(i%5===0){
-      PingPongArray.push('pong');
-    }
-    else{
-      PingPongArray.push(i);
-    }
-  }
-  return PingPongArray
+ var PingPongArray=[];
+ for (var i=0; i<inputNum; i +1){
+   if(i%15===0){
+     PingPongArray.push('pingpong');
+   }
+   
+   else if(i%3===0){
+     PingPongArray.push('ping');
+   }
+   
+   else if(i%5===0){
+     PingPongArray.push('pong');
+   }
+   
+   else{
+     PingPongArray.push(i);
+   }
+ }
+ return PingPongArray
 }
-//front end
-document.ready(function){
-$("form#pingpong").submit (function(event)){  
-  event.preventDefault;
-  var inputNum=parseInt($("input#num").val());
 
-var pingPongArray=getPingPongArray(inputNum);
+//front end
+$(document).ready(function(){
+ $("form#pingpong").submit(function(event){  
+   event.preventDefault();
+   var inputNum=parseInt($("input#num").val());
+   var PingPongArray=getPingPongArray(inputNum);
+   $("ol#pingponglist").empty();
+   PingPongArray.forEach(function(item){
+     $("ol#pingponglist").append("<li>"+item+"</li>");
+   )};
+ )};
+)};
